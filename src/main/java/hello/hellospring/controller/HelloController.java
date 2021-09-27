16b8f4ab81e1986b04dca64188a5ecb305c2ab2a
jungsik <jungsik75@gmail.com>
2021-09-28 01:04:50 +0900

    api controller append

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,9 +18,30 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloSpring(@RequestParam(value = "name") String name) {
+        return "hello " + name;//"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    @Getter
+    @Setter
+    static class Hello {
+        private String name;
     }
 
 

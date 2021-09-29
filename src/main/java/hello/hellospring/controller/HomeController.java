fd320c9102c3e0e5b36a0243e07cc31c3847ee06
jungsik <jungsik75@gmail.com>
2021-09-29 19:00:21 +0900

    createForm.html , memberList.html add

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,13 +1,48 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.LifecycleState;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberService memberService;
+
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
+    }
+
 }
